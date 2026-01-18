251dade552bd00821dd9e24be36bf99ce43462ec
tslavchev-alumni <tslavchev@jacobs-alumni.de>
2026-01-18 11:42:02 -0600

    Create 20.V_FEED_INGESTION_FRESHNESS.sql

---
+++ snowflake/gtfs_rt/20.V_FEED_INGESTION_FRESHNESS.sql
@@ -0,0 +1,15 @@
+create or replace view ANALYTICS.GTFS_RT.V_FEED_INGESTION_FRESHNESS(
+	FEED_TYPE,
+	LATEST_FETCH_TS_UTC,
+	SECONDS_SINCE_LAST_FETCH
+) as
+SELECT
+  feed_type,
+  MAX(fetch_ts_utc) AS latest_fetch_ts_utc,
+  DATEDIFF(
+    'second',
+    MAX(fetch_ts_utc),
+    CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP())::TIMESTAMP_NTZ
+  ) AS seconds_since_last_fetch
+FROM ANALYTICS.GTFS_RT.V_ENTITIES_CANONICAL
+GROUP BY feed_type;
