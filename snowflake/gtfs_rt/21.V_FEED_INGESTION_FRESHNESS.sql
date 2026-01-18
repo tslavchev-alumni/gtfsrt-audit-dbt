43f84fcbc20ebea645af9bfccf1f8509c1cff3dc
tslavchev-alumni <tslavchev@jacobs-alumni.de>
2026-01-18 12:16:01 -0600

    Update 21.V_FEED_INGESTION_FRESHNESS.sql

--- snowflake/gtfs_rt/21.V_FEED_INGESTION_FRESHNESS.sql
+++ snowflake/gtfs_rt/21.V_FEED_INGESTION_FRESHNESS.sql
@@ -1,7 +1,7 @@
 -- Is my ingestion running?
 -- 1 row per feed type
 -- Everything comes via the canonical contract/Silver boundary ANALYTICS.GTFS_RT.V_ENTITIES_CANONICAL
--- UTC standardized timezone accross the board
+-- UTC standardized timezone across the board
 
 create or replace view ANALYTICS.GTFS_RT.V_FEED_INGESTION_FRESHNESS(
 	FEED_TYPE,
